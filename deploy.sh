b9630d489c4f3a2bcdb4c4bba30e1a0fe089fc2f
Simon Brundin <simonbrundin@gmail.com>
2020-06-23 16:23:45 +0200

    Gjorde så att appen fungerar bättre med GitHub

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# if you are deploying to https://simonbrundin.github.io/ses-app
+git push -f git@github.com:simonbrundin/ses-app.git master:gh-pages
 
 cd -
