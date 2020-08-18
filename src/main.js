4921fe005c039225cab2dba00a62884034d66af9
Simon Brundin <simonbrundin@gmail.com>
2020-08-18 17:53:46 +0200

    vuex installation

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 import { routes } from './routes';
+import { store } from "./store/store";
 
 Vue.use(VueRouter);
 Vue.config.productionTip = false
@@ -12,5 +13,6 @@ const router = new VueRouter({
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
