6c01dd0498f296084abf36987fb1fd48c5f60306
Simon Brundin <simonbrundin@gmail.com>
2020-08-19 15:30:57 +0200

    Gjorde det möjligt att använda scss variabler globalt

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/ses-app/'
-    : '/'
+    : '/',
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+          @import "@/scss/style.scss";
+        `
+      }
+    }
+  }
 }
