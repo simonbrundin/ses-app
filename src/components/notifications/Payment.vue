552ac3ded79b26c2e9eb301a61485ce062e7fcaa
Simon Brundin <simonbrundin@gmail.com>
2020-08-19 15:30:29 +0200

    Skapade betalningsnotis

---
+++ src/components/notifications/Payment.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="notification-payment" v-if=" this.$store.state.dept > 0">
+    <p>
+      Det saknas en betalning från dig på
+      <strong>{{ this.$store.state.dept }} kr</strong>
+    </p>
+    <button>Betala nu</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
