508494959b86f54430bb513c6e79fffc8da3a02c
Simon Brundin <simonbrundin@gmail.com>
2020-08-20 14:33:41 +0200

    Skapade kupong

---
+++ src/components/coupons/coupon.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="coupons"></div>
+</template>
+
+<script>
+import NavigationRow from "./NavigationRow.vue";
+export default {
+  components: {
+    "navigation-row": NavigationRow,
+  },
+};
+</script>
+
+<style>
+</style>
