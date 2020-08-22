b3a5734886b8427988b6817e96183ec1fdd6eb44
Simon Brundin <simonbrundin@gmail.com>
2020-08-22 02:43:17 +0200

    Added coupons

--- src/store/store.js
+++ src/store/store.js
@@ -20,25 +20,31 @@ export const store = new Vuex.Store({
           logo: "intersport-standing.png",
           couponsLeft: 5,
           couponLink: "InterSport.vue",
-          showCompanyCoupons: true,
-          coupons: [{
-            title: '15% valfri vara',
-            description: 'Köp valfri vara och få 15% rabatt'
-          }]
+          show: true,
+          coupons: [
+            {
+              title: '15% valfri vara',
+              description: 'Köp valfri vara och få 15% rabatt'
+            },
+            {
+              title: '15% valfri vara',
+              description: 'Köp valfri vara och få 15% rabatt'
+            }
+          ]
         },
         palt: {
           name: "Palt",
           logo: "palt.png",
           couponsLeft: 5,
           couponLink: "Palt.vue",
-          showCompanyCoupons: false,
+          show: false,
         },
         biteline: {
           name: "Biteline",
           logo: "biteline.png",
           couponsLeft: 12,
           couponLink: "BiteLine.vue",
-          showCompanyCoupons: false,
+          show: false,
         },
       },
     }
