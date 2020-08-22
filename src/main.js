0f231c53339d56163d3177c73271119ffef2ee05
Simon Brundin <simonbrundin@gmail.com>
2020-08-22 02:43:27 +0200

    Added font awesome

--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import VueRouter from 'vue-router'
 import App from './App.vue'
 import { routes } from './routes';
 import { store } from "./store/store";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+import { faStore } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faHome);
+library.add(faCalendarAlt);
+library.add(faStore);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.use(VueRouter);
 Vue.config.productionTip = false
