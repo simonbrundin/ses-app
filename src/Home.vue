75b15223ec7a3216e2cdefe33c9ffafe388956ad
Simon Brundin <simonbrundin@gmail.com>
2020-10-19 14:44:20 +0200

    added auth0

--- src/Home.vue
+++ src/Home.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="home">
+    <!-- Check that the SDK client is not currently loading before accessing is methods -->
+    <div v-if="!$auth.loading">
+      <!-- show login when not authenticated -->
+      <button v-if="!$auth.isAuthenticated" @click="login">Log in</button>
+      <!-- show logout when authenticated -->
+      <button v-if="$auth.isAuthenticated" @click="logout">Log out</button>
+    </div>
     <payment-swish></payment-swish>
     <next-game></next-game>
 
@@ -20,6 +27,18 @@ export default {
     "app-bottom-menu": BottomMenu,
     "player-cash": PlayerCash,
     "payment-swish": Payment,
+  },
+  methods: {
+    // Log the user in
+    login() {
+      this.$auth.loginWithRedirect();
+    },
+    // Log the user out
+    logout() {
+      this.$auth.logout({
+        returnTo: window.location.origin,
+      });
+    },
   },
 };
 </script>
