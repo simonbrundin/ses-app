61378189e3d76c0580a3670c96b4b70421e7fc44
Simon Brundin <simonbrundin@gmail.com>
2021-03-16 16:13:46 +0100

    Updates

---
+++ _nuxt/static/1615907623/schedule/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fses-app\u002F_nuxt\u002Fstatic\u002F1615907623",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fschedule",config:{app:{basePath:"\u002Fses-app\u002F",assetsPath:"\u002Fses-app\u002F_nuxt\u002F",cdnURL:a}}}}(null));
