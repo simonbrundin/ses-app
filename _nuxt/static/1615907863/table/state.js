af728cdb6f98720343008624c5f8f6feb4c0a142
Simon Brundin <simonbrundin@gmail.com>
2021-03-16 16:17:46 +0100

    Updates

---
+++ _nuxt/static/1615907863/table/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fses-app\u002F_nuxt\u002Fstatic\u002F1615907863",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Ftable",config:{app:{basePath:"\u002Fses-app\u002F",assetsPath:"\u002Fses-app\u002F_nuxt\u002F",cdnURL:a}}}}(null));
