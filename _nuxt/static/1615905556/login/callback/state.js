56ebc0760ed191e2c3a0ef43cde7e838a7ca680c
Simon Brundin <simonbrundin@gmail.com>
2021-03-16 15:43:47 +0100

    Updates

--- _nuxt/static/1615905556/login/callback/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615905556",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Flogin\u002Fcallback",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
