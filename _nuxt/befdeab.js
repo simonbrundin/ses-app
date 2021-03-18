558335141fb3ef504a0842c2791892e2ce03f712
Simon Brundin <simonbrundin@gmail.com>
2021-03-18 22:53:04 +0100

    Updates

--- _nuxt/befdeab.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[27],{412:function(e,n,t){"use strict";t.r(n);var r={setup:function(){return{}}},c=t(2),component=Object(c.a)(r,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,"0650528e",null);n.default=component.exports}}]);
