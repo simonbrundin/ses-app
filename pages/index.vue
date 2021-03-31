18b7030e6d853012126ab0d374ac64415b60f57e
Simon Brundin <simonbrundin@gmail.com>
2021-03-31 23:20:05 +0200

    added table and improved design

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,89 @@
 <template>
   <div id="app">
-    <nuxt-link v-if="$auth.loggedIn" to="/profile">
-      <img id="profile-image" :src="$auth.user.picture" class="round" />
-    </nuxt-link>
-    <button v-else @click="$auth.loginWith('auth0')">Logga in</button>
+    <div class="top-row">
+      <div class="greeting">
+        <div class="date">{{ date }}</div>
+        <h1>Vamos, {{ $auth.user.given_name }}!</h1>
+      </div>
 
+      <nuxt-link v-if="$auth.loggedIn" to="/profile">
+        <img class="profile-image round" :src="$auth.user.picture" />
+      </nuxt-link>
+      <button v-else @click="$auth.loginWith('auth0')">Logga in</button>
+    </div>
     <div v-if="$store.state.showMenuIcon" class="menu-icon" @click="showMenu">
       Meny
     </div>
-    <Waiting v-if="$store.state.upcomingGames.length === 0" />
+    <Notifications-WaitingForPlayers
+      v-if="$store.state.upcomingGames.length === 0"
+      class="notification"
+    />
 
-    <NextGame v-if="false" />
+    <NextGame v-if="$store.state.upcomingGames.length > 0" />
+    <NextGame2 v-if="$store.state.upcomingGames.length > 0" />
     <MatchRegistration v-if="false" />
-
+    <Table v-if="$store.state.user.league" />
     <Menu v-if="$store.state.showHide.menu" />
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      dat: 'sön 23 mar',
+    };
+  },
+  computed: {
+    date() {
+      const today = new Date();
+      const date = today.getDate();
+      const month = today.getMonth();
+      let swemonth = '';
+      switch (month) {
+        case 1:
+          swemonth = 'jan';
+          break;
+        case 2:
+          swemonth = 'feb';
+          break;
+        case 3:
+          swemonth = 'mars';
+          break;
+        case 4:
+          swemonth = 'apr';
+          break;
+        case 5:
+          swemonth = 'maj';
+          break;
+        case 6:
+          swemonth = 'juni';
+          break;
+        case 7:
+          swemonth = 'juli';
+          break;
+        case 8:
+          swemonth = 'aug';
+          break;
+        case 9:
+          swemonth = 'sep';
+          break;
+        case 10:
+          swemonth = 'okt';
+          break;
+        case 11:
+          swemonth = 'nov';
+          break;
+        case 12:
+          swemonth = 'dec';
+          break;
+        default:
+          break;
+      }
+      const string = '' + date + ' ' + swemonth;
+      return string;
+    },
+  },
   mounted() {
     this.isUserInfoFilled();
   },
@@ -39,4 +104,28 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.top-row {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 0 10px 0;
+}
+.greeting {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin: 0;
+}
+.date {
+  color: $gold;
+  text-transform: uppercase;
+}
+.notification {
+  margin: 0 0 $app-padding 0;
+}
+.profile-image {
+  height: 60px;
+}
+</style>
