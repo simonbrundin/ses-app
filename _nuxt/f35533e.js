767e022ce6ceaf60608a06da50114b5a57a6ab76
Simon Brundin <simonbrundin@gmail.com>
2021-04-02 08:04:05 +0200

    Updates

---
+++ _nuxt/f35533e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{437:function(e,n,t){"use strict";t.r(n);var r={setup:function(){return{}}},c=t(0),component=Object(c.a)(r,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,"0650528e",null);n.default=component.exports}}]);
