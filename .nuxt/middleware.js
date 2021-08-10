06c4f2897585fa4cd21ea43af89bcfa3d5fdb17a
Simon Brundin <simonbrundin@gmail.com>
2021-08-10 18:26:30 +0200

    Updates

--- .nuxt/middleware.js
+++
@@ -1,6 +0,0 @@
-const middleware = {}
-
-middleware['force_login'] = require('../middleware/force_login.js')
-middleware['force_login'] = middleware['force_login'].default || middleware['force_login']
-
-export default middleware
