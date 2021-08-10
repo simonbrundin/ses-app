5b516dcb7e9813f2ca642b0ebed88d9377e4d069
Simon Brundin <simonbrundin@gmail.com>
2021-08-10 21:18:38 +0200

    Updates

--- dist/static/1628622915/slots/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdist\u002Fstatic\u002F1628622915",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fslots",config:{axios:{browserBaseURL:"http:\u002F\u002Flocalhost:4000"},_app:{basePath:"\u002F",assetsPath:"\u002Fdist\u002F",cdnURL:a}}}}(null));
