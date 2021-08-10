8bd1c67243ac69c6735e927d9948c9c4b32dc90f
Simon Brundin <simonbrundin@gmail.com>
2021-08-10 22:15:46 +0200

    Updates

---
+++ nuxt_generate/static/1628626545/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fnuxt_generate\u002Fstatic\u002F1628626545",layout:"default",error:a,state:a,serverRendered:false,routePath:b,config:{axios:{browserBaseURL:"https:\u002F\u002Fsuperelitserien-server.herokuapp.com"},_app:{basePath:b,assetsPath:"\u002Fnuxt_generate\u002F",cdnURL:a}}}}(null,"\u002F"));
