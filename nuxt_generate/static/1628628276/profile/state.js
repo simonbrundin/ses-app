de10766422c7ff84c0bcdc6bda17fd4816fe0208
Simon Brundin <simonbrundin@gmail.com>
2021-08-10 22:44:38 +0200

    Updates

---
+++ nuxt_generate/static/1628628276/profile/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt_generate\u002Fstatic\u002F1628628276",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fprofile",config:{axios:{browserBaseURL:"http:\u002F\u002Flocalhost:4000"},_app:{basePath:"\u002F",assetsPath:"\u002Fnuxt_generate\u002F",cdnURL:a}}}}(null));
