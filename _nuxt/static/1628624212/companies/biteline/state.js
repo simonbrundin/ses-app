1acbefb6defcd2c48b60c1d8e86253a31a3623e5
Simon Brundin <simonbrundin@gmail.com>
2021-08-10 21:36:53 +0200

    Updates

---
+++ _nuxt/static/1628624212/companies/biteline/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1628624212",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fcompanies\u002Fbiteline",config:{axios:{browserBaseURL:"http:\u002F\u002Flocalhost:4000"},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
