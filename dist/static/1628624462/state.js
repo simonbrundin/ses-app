ae26e9826deb2a09f62189b58f3ed1d92d2116ee
Simon Brundin <simonbrundin@gmail.com>
2021-08-10 21:41:04 +0200

    Updates

---
+++ dist/static/1628624462/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fdist\u002Fstatic\u002F1628624462",layout:"default",error:a,state:a,serverRendered:false,routePath:b,config:{axios:{browserBaseURL:"http:\u002F\u002Flocalhost:4000"},_app:{basePath:b,assetsPath:"\u002Fdist\u002F",cdnURL:a}}}}(null,"\u002F"));
---
+++ dist/static/1628624462/table/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdist\u002Fstatic\u002F1628624462",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Ftable",config:{axios:{browserBaseURL:"http:\u002F\u002Flocalhost:4000"},_app:{basePath:"\u002F",assetsPath:"\u002Fdist\u002F",cdnURL:a}}}}(null));
