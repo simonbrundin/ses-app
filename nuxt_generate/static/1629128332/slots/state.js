fd938d28292291316c0152c01c4f41911bb77abc
Simon Brundin <simonbrundin@gmail.com>
2021-08-16 17:38:54 +0200

    Updates

---
+++ nuxt_generate/static/1629128332/slots/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt_generate\u002Fstatic\u002F1629128332",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fslots",config:{_app:{basePath:"\u002F",assetsPath:"\u002Fnuxt_generate\u002F",cdnURL:a}}}}(null));
