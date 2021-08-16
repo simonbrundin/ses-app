2b75704bbd121306cb32e689e5963a3eb09e308b
Simon Brundin <simonbrundin@gmail.com>
2021-08-16 18:11:43 +0200

    Updates

--- nuxt_generate/static/1629130060/companies/Coupon/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt_generate\u002Fstatic\u002F1629130060",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fcompanies\u002FCoupon",config:{_app:{basePath:"\u002F",assetsPath:"\u002Fnuxt_generate\u002F",cdnURL:a}}}}(null));
