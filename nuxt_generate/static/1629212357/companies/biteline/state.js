f3de1d9e08265da95aeae7bfed79750509f6e812
Simon Brundin <simonbrundin@gmail.com>
2021-08-17 17:04:19 +0200

    Updates

--- nuxt_generate/static/1629212357/companies/biteline/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt_generate\u002Fstatic\u002F1629212357",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fcompanies\u002Fbiteline",config:{axios:{browserBaseURL:"https:\u002F\u002Fsuperelitserien-server.herokuapp.com"},_app:{basePath:"\u002F",assetsPath:"\u002Fnuxt_generate\u002F",cdnURL:a}}}}(null));
---
+++ nuxt_generate/static/1629212657/companies/InterSport/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt_generate\u002Fstatic\u002F1629212657",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fcompanies\u002FInterSport",config:{axios:{browserBaseURL:"https:\u002F\u002Fsuperelitserien-server.herokuapp.com"},_app:{basePath:"\u002F",assetsPath:"\u002Fnuxt_generate\u002F",cdnURL:a}}}}(null));
