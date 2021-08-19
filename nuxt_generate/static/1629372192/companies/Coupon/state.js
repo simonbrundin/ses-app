0e15e11e1984076652b1088a6226531afce618dc
Simon Brundin <simonbrundin@gmail.com>
2021-08-19 13:25:46 +0200

    Updates

--- nuxt_generate/static/1629372192/companies/Coupon/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt_generate\u002Fstatic\u002F1629372192",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fcompanies\u002FCoupon",config:{axios:{browserBaseURL:"https:\u002F\u002Fsuperelitserien-server.herokuapp.com"},_app:{basePath:"\u002F",assetsPath:"\u002Fnuxt_generate\u002F",cdnURL:a}}}}(null));
